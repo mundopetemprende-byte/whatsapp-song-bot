f096d3932457b6204252796cfc673e1d6bd1db67
mundopetemprende-byte <mundopetemprende@gmail.com>
2026-03-09 16:13:59 -0500

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -1,1 +1,29 @@
+require("dotenv").config();
+const axios = require("axios");
 
+async function sendMessage(phone,text){
+
+await axios.post(
+
+`https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`,
+
+{
+messaging_product: "whatsapp",
+to: phone,
+text: {
+body: text
+}
+},
+
+{
+headers:{
+Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+"Content-Type":"application/json"
+}
+}
+
+);
+
+}
+
+module.exports = sendMessage;
