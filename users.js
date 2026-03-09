9abadaad5c5754ece6f6b979c1e8785866a4231c
mundopetemprende-byte <mundopetemprende@gmail.com>
2026-03-09 16:12:48 -0500

    Update users.js

--- users.js
+++ users.js
@@ -1,1 +1,28 @@
+const users = {};
 
+function getUser(phone){
+
+if(!users[phone]){
+users[phone] = {
+state: "nuevo"
+};
+}
+
+return users[phone];
+
+}
+
+function setState(phone,state){
+
+if(!users[phone]){
+users[phone] = {};
+}
+
+users[phone].state = state;
+
+}
+
+module.exports = {
+getUser,
+setState
+};
