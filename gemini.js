923326f1d3a47df601961d94a4b34a08bd0340dd
mundopetemprende-byte <mundopetemprende@gmail.com>
2026-03-09 16:13:40 -0500

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -1,1 +1,29 @@
+require("dotenv").config();
+const axios = require("axios");
+const prompt = require("./prompt");
 
+async function askGemini(message){
+
+const response = await axios.post(
+
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
+
+{
+contents:[
+{
+parts:[
+{
+text: prompt + "\nCliente: " + message
+}
+]
+}
+]
+}
+
+);
+
+return response.data.candidates[0].content.parts[0].text;
+
+}
+
+module.exports = askGemini;
